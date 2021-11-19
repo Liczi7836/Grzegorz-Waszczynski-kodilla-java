8d4fe1b75ef253abbcacced3fc1e5a5d738f8fb1
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2021-11-19 18:48:00 +0100

    7.3

--- kodilla-testing/src/main/java/com/kodilla/stream/iterate/StreamMain.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/iterate/StreamMain.java
@@ -1,8 +1,24 @@
 package com.kodilla.stream.iterate;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args){
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        Forum forum = new Forum();
+
+        Map<Integer, ForumUser> theResultMapOfUsers = forum.getList().stream()
+                .filter(ForumUser -> ForumUser.getSex() == 'M')
+                .filter(ForumUser -> ForumUser.getBirthday().plusYears(20).isBefore(LocalDate.now()))
+                .filter(ForumUser -> ForumUser.getPostsNumber() > 0)
+                .collect(Collectors.toMap(ForumUser::getIdentifier, ForumUser -> ForumUser));
+
+        System.out.println("# elements: " + theResultMapOfUsers.size());
+        theResultMapOfUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
