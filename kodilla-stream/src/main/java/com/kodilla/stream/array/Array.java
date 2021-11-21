c14d8ce0358f6a61e107b6cb8bb0c6498ea61709
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2021-11-21 18:33:24 +0100

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/Array.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/Array.java
@@ -1,23 +1,16 @@
 package com.kodilla.stream.array;
 
-import java.util.Arrays;
+
 import java.util.stream.IntStream;
 
 public final class Array implements ArrayOperations {
-    private final int[] numbers;
-
-    public Array(final int[] numbers) {
-        this.numbers = numbers;
-    }
 
 
 
-    @Override
-    public String toString() {
-        return "Array{" +
-                "numbers=" + Arrays.toString(numbers) +
-                '}';
-    }
+
+
+
+
 
     public double getAverage(int[] numbers){
             IntStream.range(0, numbers.length)
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -19,7 +19,7 @@ public class ArrayOperationsTestSuite {
 
 
 
-        Array array = new Array(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,20});
+        Array array = new Array();
         System.out.println(array);
         //When
         double average = array.getAverage(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,20});
