0d9e3615da78f15ffac509ed82614de22fcb88fb
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2021-12-01 19:06:48 +0100

    6.7

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculatingStatistics.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.List;
+
+public class CalculatingStatistics {
+
+    private Statistics statistics;
+
+    List<String> names;
+    int posts;
+    int comments;
+
+    public CalculatingStatistics(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics){
+
+
+
+    }
+}
