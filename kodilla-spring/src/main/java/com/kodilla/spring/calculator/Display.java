cb414141dd4606a9b789bb354693d3ec8e051716
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2021-12-12 13:24:21 +0100

    10.2

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.calculator;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public final class Display {
+
+
+    public void displayValue(double val){
+        System.out.println("Value is: " + val);
+    }
+}
