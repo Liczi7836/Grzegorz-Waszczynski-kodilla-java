5da9745bbceadc89875df920a0f0e41acd853906
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2022-01-14 16:20:00 +0100

    13.2

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -3,11 +3,13 @@ package com.kodilla.hibernate.tasklist.dao;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class TaskListDaoTestSuite {
 
     @Autowired
