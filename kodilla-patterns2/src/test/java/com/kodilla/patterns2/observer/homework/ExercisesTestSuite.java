a12e9857dd99832698eb253f13bedf87881cec26
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2022-03-07 16:41:18 +0100

    20.4

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/ExercisesTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/ExercisesTestSuite.java
@@ -15,12 +15,16 @@ public class ExercisesTestSuite {
         Exercises batMan = new FourthStudent();
         Mentor jacek = new Mentor("Jacek");
         Mentor piotr = new Mentor("Piotr");
+        Mentor klaudiusz = new Mentor("Klaudiusz");
         johnWick.registerObserver(jacek);
         mikeWazowsky.registerObserver(jacek);
         superMan.registerObserver(piotr);
         mikeWazowsky.registerObserver(piotr);
         batMan.registerObserver(jacek);
         batMan.registerObserver(piotr);
+        batMan.registerObserver(klaudiusz);
+        johnWick.registerObserver(klaudiusz);
+        johnWick.removeObserver(klaudiusz);
         //When
         johnWick.addExercise(new Exercise("Java!", "Programming java app"));
         johnWick.addExercise(new Exercise("HTML!", "Programming websites with html"));
@@ -30,9 +34,13 @@ public class ExercisesTestSuite {
         superMan.addExercise(new Exercise("Chemistry for A+", "How to not create bomb"));
         batMan.addExercise(new Exercise("FrontEnd", "Programming website"));
         batMan.addExercise(new Exercise("PE", "Record jumping for 2h"));
+        johnWick.notifyObservers();
+        mikeWazowsky.notifyObservers();
+        superMan.notifyObservers();
+        batMan.notifyObservers();
         //Then
         assertEquals(5, jacek.getCount());
         assertEquals(6, piotr.getCount());
-
+        assertEquals(2, klaudiusz.getCount());
     }
 }
