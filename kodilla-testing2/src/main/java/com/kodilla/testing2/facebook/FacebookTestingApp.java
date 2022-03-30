1b858e18b0cec6ec49fdd407f18e5abb55b4de3b
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2022-03-30 14:13:48 +0200

    23.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -8,28 +8,24 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
 
-    public static final String XPATH_NEWACCOUNT = "//div[contains(@class, \"_6luv _52jv\")]/form/div/button[2]";
-    public static final String XPATH_DAY = "//div[contains(@class, \"_5k_5\")]/select[1]";
-    public static final String XPATH_MONTH = "//div[contains(@class, \"_5k_5\")]/select[2]";
-    public static final String XPATH_YEAR = "//div[contains(@class, \"_5k_5\")]/select[3]";
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com/");
 
         driver.findElement(By.xpath("//button[2]")).click();
-        driver.findElement(By.xpath("//button[2]")).click();
-        //driver.findElement(By.xpath(XPATH_NEWACCOUNT)).click()
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a")).click();
 
-        WebElement selectCombo1 = driver.findElement(By.xpath(XPATH_DAY));
+        Thread.sleep(5000);
+
+        WebElement selectCombo1 = driver.findElement(By.id("day"));
         Select selectDay = new Select(selectCombo1);
         selectDay.selectByValue("28");
 
-        WebElement selectCombo2 = driver.findElement(By.xpath(XPATH_MONTH));
+        WebElement selectCombo2 = driver.findElement(By.id("month"));
         Select selectMonth = new Select(selectCombo2);
         selectMonth.selectByIndex(9);
 
-        WebElement selectCombo3 = driver.findElement(By.xpath(XPATH_YEAR));
+        WebElement selectCombo3 = driver.findElement(By.id("year"));
         Select selectYear = new Select(selectCombo3);
         selectYear.selectByValue("1995");
     }
